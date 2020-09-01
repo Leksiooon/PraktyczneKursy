c218b626beed3433640a8bf93d0952aed9dd8db7
Leksiooon <sierocinskip.maciej@gmail.com>
2020-09-01 15:02:27 +0200

    dodanie atrybutów do klas naszego modelu

--- PraktyczneKursy/PraktyczneKursy/Models/Kategoria.cs
+++ PraktyczneKursy/PraktyczneKursy/Models/Kategoria.cs
@@ -1,12 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace PraktyczneKursy.Models
 {
     public class Kategoria
     {
         public int KategoriaId { get; set; }
+        [Required(ErrorMessage = "Wprowadz nazwę kategorii")]
+        [StringLength(100)]
         public string NazwaKategorii { get; set; }
+        [Required(ErrorMessage = "Wprowadz opis kategorii")]
+        [StringLength(100)]
         public string OpisKategorii { get; set; }
         public string NazwaPlikuIkony { get; set; }
 
