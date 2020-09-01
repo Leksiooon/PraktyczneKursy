2c11d64df4fcc848f0d3edaf91f1e7f3f2a5b729
Leksiooon <sierocinskip.maciej@gmail.com>
2020-09-01 17:32:16 +0200

    dodanie KursyContext

---
+++ PraktyczneKursy/PraktyczneKursy/DAL/KursyContext.cs
@@ -0,0 +1,30 @@
+﻿using PraktyczneKursy.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace PraktyczneKursy.DAL
+{
+    public class KursyContext : DbContext
+    {
+        public KursyContext() : base("KursyContext")
+        { }
+
+        public DbSet<Kurs> Kursy { get; set; }
+        public DbSet<Kategoria> Kategorie { get; set; }
+        public DbSet<Zamowienie> Zamowienia { get; set; }
+        public DbSet<PozycjaZamowienia> PozycjeZamowienia { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //wyłącza konwencję dopisującą na koniec nazwy tabeli końcówkę -s
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+
+    }
+}
